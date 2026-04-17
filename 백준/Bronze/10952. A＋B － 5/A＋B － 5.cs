fbab343fc9de40fa97c7250ed248d4d973fdc22e
정재석 <110978146+Jass2345@users.noreply.github.com>
2026-04-17 17:28:26 +0900

    [Bronze V] Title: A+B - 5, Time: 56 ms, Memory: 5524 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.cs
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cs
@@ -1,11 +1,6 @@
-int A = 0;
-int B = 0;
-
 while(true)
 {
-    string[] num = (Console.ReadLine().Split(' '));
-    A = int.Parse(num[0]);
-    B = int.Parse(num[1]);
-    if (A == 0 && B == 0) break;
-    else Console.WriteLine(A + B);
+    int[] num = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+    if (num[0] == 0 && num[1] == 0) break;
+    else Console.WriteLine(num[0] + num[1]);
 }
